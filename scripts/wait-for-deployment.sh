f7d7a0baf95952f6d0b33277bd86d9ec55b43e68
Sean Sundberg <seansund@us.ibm.com>
2021-07-07 22:07:01 -0500

    Updates the wait for deployment logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/wait-for-deployment.sh
+++ scripts/wait-for-deployment.sh
@@ -3,10 +3,17 @@
 NAMESPACE="$1"
 NAME="$2"
 
+count=0
+until kubectl get deployment -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep -q "${NAME}" || [[ $count -eq 15 ]]; do
+  count=$((count + 1))
+  sleep 15
+done
+
 DEPLOYMENT_NAME=$(kubectl get deployment -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep "${NAME}" | head -1)
 
 if [[ -z "${DEPLOYMENT_NAME}" ]]; then
   echo "Unable to find deployment: ${NAME}"
+  kubectl get deployment -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
   exit 1
 fi
 
