82520ddb5a6bc9c0de723ed5378380eb86c6cfac
Sean Sundberg <seansund@us.ibm.com>
2021-07-08 18:33:55 -0500

    Tests for existing cluster rbac before provisioning instance
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-instance.sh
+++ scripts/create-instance.sh
@@ -14,7 +14,13 @@ if kubectl explain sealedsecretcontroller.bitnami.com 1> /dev/null 2> /dev/null;
   CREATE_CRD="false"
 fi
 
+CREATE_RBAC="true"
+if kubectl get clusterrole secrets-unsealer 1> /dev/null 2> /dev/null; then
+  CREATE_RBAC="false"
+fi
+
 cat "${CONFIG_DIR}/instance.yaml" | \
   sed "s/CREATE_CRD/${CREATE_CRD}/g" | \
+  sed "s/CREATE_RBAC/${CREATE_RBAC}/g" | \
   sed "s/SEALED_SECRET_KEY/${SECRET_NAME}/g" | \
   kubectl apply -n "${NAMESPACE}" -f -
