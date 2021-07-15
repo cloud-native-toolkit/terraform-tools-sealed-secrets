123f8b9ebc928962c4d53b094ea23530f1ece2df
Sean Sundberg <seansund@us.ibm.com>
2021-07-15 12:49:53 -0500

    Fixes public_key output value and adds cert to output (#6)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
 output "public_key" {
-  value = local.private_key
+  value = local.public_key
 }
 
 output "private_key" {
   value = local.private_key
   sensitive = true
+}
+
+output "cert" {
+  value = tls_self_signed_cert.cert.cert_pem
 }
