f7c08b05bce94392d430eecaabb5a0f0488c178e
Sean Sundberg <seansund@us.ibm.com>
2021-07-28 14:43:33 -0500

    Updates outputs to wait for deployment
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
 output "public_key" {
   value = local.public_key
+  depends_on = [null_resource.wait_for_deployment]
 }
 
 output "private_key" {
   value = local.private_key
   sensitive = true
+  depends_on = [null_resource.wait_for_deployment]
 }
 
 output "cert" {
   value = tls_self_signed_cert.cert.cert_pem
+  depends_on = [null_resource.wait_for_deployment]
 }
