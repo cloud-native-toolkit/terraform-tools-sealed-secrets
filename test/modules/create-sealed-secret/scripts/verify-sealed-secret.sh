b6bd96688586ef4a38c72c0528fc53c339e214ee
Sean Sundberg <seansund@us.ibm.com>
2021-07-28 14:47:58 -0500

    Extract secret value
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/modules/create-sealed-secret/scripts/verify-sealed-secret.sh
+++ test/modules/create-sealed-secret/scripts/verify-sealed-secret.sh
@@ -5,4 +5,6 @@ NAMESPACE="$2"
 
 kubectl describe sealedsecret "${SECRET_NAME}" -n "${NAMESPACE}"
 
-kubectl get secret "${SECRET_NAME}" -n "${NAMESPACE}"
+kubectl get secret "${SECRET_NAME}" -n "${NAMESPACE}" || exit 1
+
+oc extract "secret/${SECRET_NAME}" -n "${NAMESPACE}" --to=-
