196b34aff86f77a0cdce60c7680fdad8bbc28bde
Sean Sundberg <seansund@us.ibm.com>
2021-11-13 08:33:32 -0600

    Adds test so delete-tls-secret doesn't fail if secret missing
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/delete-tls-secret.sh
+++ scripts/delete-tls-secret.sh
@@ -3,4 +3,8 @@
 NAMESPACE="$1"
 SECRET_NAME="$2"
 
-kubectl delete secret -n "${NAMESPACE}" "${SECRET_NAME}"
+if kubectl get secret -n "${NAMESPACE}" "${SECRET_NAME}"; then
+  kubectl delete secret -n "${NAMESPACE}" "${SECRET_NAME}"
+else
+  echo "Secret ${NAMESPACE}/${SECRET_NAME} not found"
+fi
