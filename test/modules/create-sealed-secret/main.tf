fe61c1667bde434f4652b6d57b720091737fb7fa
Sean Sundberg <seansund@us.ibm.com>
2022-03-07 12:30:41 -0600

    Updates test submodule to use clis submodule
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/modules/create-sealed-secret/main.tf
+++ test/modules/create-sealed-secret/main.tf
@@ -7,6 +7,12 @@ locals {
   secret_file = "${local.tmp_dir}/sealed-secret.json"
 }
 
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+
+  clis = ["oc", "kubectl", "kubeseal"]
+}
+
 resource local_file cert_file {
   filename = local.cert_file
 
@@ -16,6 +22,10 @@ resource local_file cert_file {
 resource null_resource create_sealed_secret {
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-sealed-secret.sh '${local.secret_name}' '${var.namespace}' '${local.secret_value}' '${local_file.cert_file.filename}' '${local.secret_file}'"
+
+    environment = {
+      BIN_DIR = module.setup_clis.bin_dir
+    }
   }
 }
 
@@ -23,7 +33,7 @@ resource null_resource apply_sealed_secret {
   depends_on = [null_resource.create_sealed_secret]
 
   provisioner "local-exec" {
-    command = "kubectl apply -n ${var.namespace} -f ${local.secret_file}"
+    command = "${module.setup_clis.bin_dir}/kubectl apply -n ${var.namespace} -f ${local.secret_file}"
 
     environment = {
       KUBECONFIG = var.kubeconfig
@@ -38,6 +48,7 @@ resource null_resource verify_secret {
     command = "${path.module}/scripts/verify-sealed-secret.sh ${local.secret_name} ${var.namespace}"
 
     environment = {
+      BIN_DIR = module.setup_clis.bin_dir
       KUBECONFIG = var.kubeconfig
     }
   }
