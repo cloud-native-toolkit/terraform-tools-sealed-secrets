4d9e70828e30795cfd186363c24546fbf77f3257
Sean Sundberg <seansund@us.ibm.com>
2022-03-23 14:24:14 -0500

    Adds namespace to the outputs
    Closes #19
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "cert" {
   value = var.cert
   depends_on = [null_resource.create_instance]
 }
+
+output "namespace" {
+  value = var.namespace
+  depends_on = [null_resource.create_namespace]
+}
