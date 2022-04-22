143b57b724d934908985f632506ab24feb7f139b
Andrew Trice <amtrice@us.ibm.com>
2022-04-22 11:35:47 -0400

    updated namespace references
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-sealed-secrets.tf
+++ test/stages/stage2-sealed-secrets.tf
@@ -4,13 +4,15 @@ module "sealed_secrets" {
   cert                = module.cert.cert
   private_key         = module.cert.private_key
   cluster_config_file = module.dev_cluster.config_file_path
-  namespace           = module.dev_capture_tools_state.namespace
+  #namespace           = module.dev_capture_tools_state.namespace
+  namespace          = module.dev_tools_namespace.name
 }
 
 module "verify_sealed_secrets" {
   source = "./module/test/modules/create-sealed-secret"
 
   kubeconfig         = module.dev_cluster.config_file_path
-  namespace          = module.dev_capture_tools_state.namespace
+  #namespace          = module.dev_capture_tools_state.namespace
+  namespace          = module.dev_tools_namespace.name
   sealed_secret_cert = module.sealed_secrets.cert
 }
